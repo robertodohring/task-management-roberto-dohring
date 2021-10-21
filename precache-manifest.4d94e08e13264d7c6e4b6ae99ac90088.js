5c52e444ad7b0ca0c64e32f9c2e2e984b6d59ad6
robertodohring <robertodohring@users.noreply.github.com>
2021-10-21 21:12:22 +0000

    deploy: e7a8335b9f49821d36137e838ef7974fba919e81

---
+++ precache-manifest.4d94e08e13264d7c6e4b6ae99ac90088.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d2a352dffaa1017cd6e29936a27fca6",
+    "url": "https://robertodohring.github.io/task-management-roberto-dohring/index.html"
+  },
+  {
+    "revision": "bfda5d56ed45abfe0a36",
+    "url": "https://robertodohring.github.io/task-management-roberto-dohring/static/css/main.865def11.chunk.css"
+  },
+  {
+    "revision": "d77154b494f2ad7e1c61",
+    "url": "https://robertodohring.github.io/task-management-roberto-dohring/static/js/2.1648a13d.chunk.js"
+  },
+  {
+    "revision": "bfda5d56ed45abfe0a36",
+    "url": "https://robertodohring.github.io/task-management-roberto-dohring/static/js/main.ae131d85.chunk.js"
+  },
+  {
+    "revision": "3fb4574558574dd244bf",
+    "url": "https://robertodohring.github.io/task-management-roberto-dohring/static/js/runtime~main.e480b207.js"
+  }
+]);
